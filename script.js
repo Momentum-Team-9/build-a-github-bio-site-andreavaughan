74b33d866c2e175ccd64810fbff57ee8ecb789ab
andreavaughan <amvaughan16@gmail.com>
2021-07-22 08:50:10 -0400

    fixed flexbox styling issue

--- script.js
+++ script.js
@@ -11,44 +11,58 @@ fetch('https://api.github.com/users/andreavaughan')
     myName.innerText = data.name
     infoCard.appendChild(myName)
 
-    // const about = document.getElementById('about')
-    // infoCard.classList.add('info-card')
+    const about = document.createElement('div')
+    about.setAttribute('id', 'about')
+    about.classList.add('card')
+    infoCard.appendChild(about)
+
+    const aboutDiv = document.getElementById('about')
+    const aboutLabel = document.createElement('h2')
+    aboutLabel.innerText = 'About'
+    aboutDiv.appendChild(aboutLabel)
 
     const nameParagraph = document.createElement('p')
-    nameParagraph.innerText = 'Name ' + data.name
-    infoCard.appendChild(nameParagraph)
+    nameParagraph.innerText = 'Name: ' + data.name
+    aboutDiv.appendChild(nameParagraph)
 
     const gitHubLink = document.createElement('a')
-    const linkOne = document.createTextNode('GitHub URL ' + data.html_url)
+    const linkOne = document.createTextNode('GitHub URL: ' + data.html_url)
     gitHubLink.appendChild(linkOne)
     gitHubLink.href = data.html_url
     gitHubLink.target = "_blank"
-    infoCard.appendChild(gitHubLink)
+    aboutDiv.appendChild(gitHubLink)
 
     const company = document.createElement('p')
     company.innerText = 'Company: ' + data.company
-    infoCard.appendChild(company)
+    aboutDiv.appendChild(company)
 
     const website = document.createElement('a')
-    const linkTwo = document.createTextNode('Website ' + data.blog)
+    const linkTwo = document.createTextNode('Website: ' + data.blog)
     website.appendChild(linkTwo)
     website.href = data.blog
     website.target = "_blank"
-    infoCard.appendChild(website)
+    aboutDiv.appendChild(website)
 
-    // const bio = document.getElementById('bio')
-    // bio.classList.add('info-card')
+    const bio = document.createElement('div')
+    bio.setAttribute('id', 'bio')
+    bio.classList.add('card')
+    infoCard.appendChild(bio)
+
+    const bioDiv = document.getElementById('bio')
+    const bioLabel = document.createElement('h2')
+    bioLabel.innerText = 'Bio'
+    bioDiv.appendChild(bioLabel)
 
     const bioText = document.createElement('p')
     bioText.innerText = data.bio
-    infoCard.appendChild(bioText)
+    bioDiv.appendChild(bioText)
 
     const picture = document.createElement('img')
     picture.src = data.avatar_url
     console.log(picture.src)
-    infoCard.appendChild(picture)
+    bioDiv.appendChild(picture)
 
-    fetch(data.repos_url) //this is called a nested api, it's an api request within an api request
+    fetch(data.repos_url) 
         .then(res => res.json())
         .then(repos => {
             for (repo of repos){
