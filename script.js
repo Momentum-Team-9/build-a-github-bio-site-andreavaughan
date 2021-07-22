8af1156bf533237a784bcd511bf22b4fb907f2fa
andreavaughan <amvaughan16@gmail.com>
2021-07-21 22:01:39 -0400

    working out flex styling

--- script.js
+++ script.js
@@ -11,42 +11,42 @@ fetch('https://api.github.com/users/andreavaughan')
     myName.innerText = data.name
     infoCard.appendChild(myName)
 
-    const about = document.getElementById('about')
-    about.classList.add('info-card')
+    // const about = document.getElementById('about')
+    // infoCard.classList.add('info-card')
 
     const nameParagraph = document.createElement('p')
     nameParagraph.innerText = 'Name ' + data.name
-    about.appendChild(nameParagraph)
+    infoCard.appendChild(nameParagraph)
 
     const gitHubLink = document.createElement('a')
     const linkOne = document.createTextNode('GitHub URL ' + data.html_url)
     gitHubLink.appendChild(linkOne)
     gitHubLink.href = data.html_url
     gitHubLink.target = "_blank"
-    about.appendChild(gitHubLink)
+    infoCard.appendChild(gitHubLink)
 
     const company = document.createElement('p')
     company.innerText = 'Company: ' + data.company
-    about.appendChild(company)
+    infoCard.appendChild(company)
 
     const website = document.createElement('a')
     const linkTwo = document.createTextNode('Website ' + data.blog)
     website.appendChild(linkTwo)
     website.href = data.blog
     website.target = "_blank"
-    about.appendChild(website)
+    infoCard.appendChild(website)
 
-    const bio = document.getElementById('bio')
-    bio.classList.add('info-card')
+    // const bio = document.getElementById('bio')
+    // bio.classList.add('info-card')
 
     const bioText = document.createElement('p')
     bioText.innerText = data.bio
-    bio.appendChild(bioText)
+    infoCard.appendChild(bioText)
 
     const picture = document.createElement('img')
     picture.src = data.avatar_url
     console.log(picture.src)
-    bio.appendChild(picture)
+    infoCard.appendChild(picture)
 
     fetch(data.repos_url) //this is called a nested api, it's an api request within an api request
         .then(res => res.json())
@@ -54,7 +54,7 @@ fetch('https://api.github.com/users/andreavaughan')
             for (repo of repos){
                 let repoName = document.createElement('h3')
                 repoName.innerText = repo.name
-                bio.appendChild(repoName)
+                infoCard.appendChild(repoName)
             }
         })
 })  
